6fbea1d46f3351825d72bc35d3431da8bcc5268e
Tom Coakes <tomcoakes@gmail.com>
2016-12-14 10:40:20 +0000

    simplify work-queues example

--- work-queues/createNewTask.js
+++ work-queues/createNewTask.js
@@ -2,13 +2,12 @@ var amqp = require('amqplib/callback_api');
 
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
-    var queue = 'myWorkerQueue';
-    var msg = process.argv.slice(2).join(' ') || "Hello World!";
+    var msg = process.argv.slice(2).join(' ');
 
-    ch.assertQueue(queue, {durable: true});
+    ch.assertQueue('myWorkerQueue', {durable: true});
 
-    ch.sendToQueue(queue, new Buffer(msg), {persistent: true});
-    console.log('Sent %s to the queue named: %s', msg, queue);
+    ch.sendToQueue('myWorkerQueue', new Buffer(msg), {persistent: true});
+    console.log('Sent %s to myWorkerQueue', msg);
   });
 
   setTimeout(function() {
--- work-queues/worker.js
+++ work-queues/worker.js
@@ -2,13 +2,12 @@ var amqp = require('amqplib/callback_api');
 
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
-    var queue = 'myWorkerQueue';
 
-    ch.assertQueue(queue, {durable: true});
+    ch.assertQueue('myWorkerQueue', {durable: true});
     ch.prefetch(1);
 
-    console.log('Waiting for messages to appear on %s', queue)
-    ch.consume(queue, function(msg) {
+    console.log('Waiting for messages to appear myWorkerQueue');
+    ch.consume('myWorkerQueue', function(msg) {
       var secs = msg.content.toString().split('.').length - 1;
 
       console.log('[*] Received %s', msg.content.toString());
