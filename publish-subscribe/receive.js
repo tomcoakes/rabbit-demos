50245cb5f1a3db3d43d56b6f55221e08d189972a
Tom Coakes <tomcoakes@gmail.com>
2016-12-14 10:39:09 +0000

    simplify publish-subscribe example

--- publish-subscribe/receive.js
+++ publish-subscribe/receive.js
@@ -2,12 +2,10 @@ var amqp = require('amqplib/callback_api');
 
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
-    var loggingExchange = 'myLoggingExchange';
-
-    ch.assertExchange(loggingExchange, 'fanout', {durable: false});
+    ch.assertExchange('myLoggingExchange', 'fanout', {durable: false});
 
     ch.assertQueue('', {exclusive: true}, function(err, queue) {
-      ch.bindQueue(queue.queueName, loggingExchange, '');
+      ch.bindQueue(queue.queueName, 'myLoggingExchange', '');
 
       ch.consume(queue.queueName, function(msg) {
         console.log('[x] Received %s', msg.content.toString());
--- publish-subscribe/send.js
+++ publish-subscribe/send.js
@@ -2,12 +2,10 @@ var amqp = require('amqplib/callback_api');
 
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
-    var loggingExchange = 'myLoggingExchange';
+    ch.assertExchange('myLoggingExchange', 'fanout', {durable: false});
 
-    ch.assertExchange(loggingExchange, 'fanout', {durable: false});
-
-    ch.publish(loggingExchange, '', new Buffer('Hello World!')); // empty string as second parameter means we don't want to send to any specific queue, only to the logs exchange
-    console.log('[x] Sent message to the exchange named: %s', loggingExchange);
+    ch.publish('myLoggingExchange', '', new Buffer('Hello World!')); // empty string as second parameter means we don't want to send to any specific queue, only to the logs exchange
+    console.log('[x] Sent message to myLoggingExchange');
   });
 
   setTimeout(function() {
